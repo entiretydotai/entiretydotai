95731799c8e38c9b9106bd37029070aeddbdaecd
uv <u6yuvi@gmail.com>
2020-03-21 23:41:10 +0530

    adding project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Unified repository for accessing state of the art approaches to solve Computer Vision and Natural Language Task problems using python',
+    author='Uv Aman',
+    license='MIT',
+)
